fc0e903d25ef80367189d9ab8b8528b9a820186e
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2023-06-12 18:07:37 -0300

    Hello World
    Criacao de 2 Hello World, um com React, e outro com Vite + React

---
+++ Secao01/hello-world-2/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello World, React + Vite</h1>
+      <h2>Emmet Funcionou!</h2>
+    </>
+  )
+}
+
+export default App
