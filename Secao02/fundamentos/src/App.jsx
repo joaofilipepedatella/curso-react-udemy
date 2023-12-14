9e9ca0086a676b28b99e689148afbdcb6c2d8912
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2023-12-14 13:42:37 -0300

    exclusao de codigos desnecessarios

--- Secao02/fundamentos/src/App.jsx
+++ Secao02/fundamentos/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 //Components
@@ -12,8 +10,6 @@ import Events from './components/Events'
 import Challenge from './components/Challenge'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Fundamentos React</h1>
