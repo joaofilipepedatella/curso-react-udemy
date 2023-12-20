7d830993eb52dad6c966ba04ae9ed3b5b5479c25
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2023-12-20 14:37:03 -0300

    Criando componentes do projeto

---
+++ Secao06-projeto-secret-word/secret-word-project/src/components/StartScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <div className='start'>
+        <h1>Secret Word</h1>
+        <p>Clique no botao abaixo para comecar a jogar</p>
+        <button>Comecar o jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen
