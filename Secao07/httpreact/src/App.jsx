172cea9f9269eff2b75e0cb8385f5ec8bf1e5a9b
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2023-12-22 16:26:42 -0300

    inclusao da pasta hooks, insercao de hooks customs e utilizacao do mesmo

--- Secao07/httpreact/src/App.jsx
+++ Secao07/httpreact/src/App.jsx
@@ -3,30 +3,37 @@ import './App.css'
 
 import { useState, useEffect } from "react"
 
+// 4 - custom hook
+
+import { useFetch } from './hooks/useFetch'
+
 const url = "http://localhost:3000/products"
 
 function App() {
 
   const [products, setProducts] = useState([])
 
+  // 4 - custom hook
+  const { data: items } = useFetch(url)
+
   const [name, setName] = useState("")
   const [price, setPrice] = useState("")
 
   // 1 - Resgatando dados
 
-  useEffect(() => {
-
-    async function fetchData() {
-      const res = await fetch(url)
-
-      const data = await res.json()
-
-      setProducts(data)
-
-    }
-
-    fetchData()
-  }, [])
+  /*   useEffect(() => {
+  
+      async function fetchData() {
+        const res = await fetch(url)
+  
+        const data = await res.json()
+  
+        setProducts(data)
+  
+      }
+  
+      fetchData()
+    }, []) */
 
   // 2 - Adicionando produtos
 
@@ -63,7 +70,7 @@ function App() {
     <>
       <h1>Lista de Produtos</h1>
       <ul>
-        {products.map((product) =>
+        {items && items.map((product) =>
         (
           <li key={product.id}>{product.name} - R$: {product.price} </li>
         ))}
