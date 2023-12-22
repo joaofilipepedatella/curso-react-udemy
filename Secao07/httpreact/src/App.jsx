6dd5149cd3276632af76c10536ddce401a982837
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2023-12-22 15:50:01 -0300

    Envio de aulas sobre adicao de produtos, e adicao dinamica, com limpeza dos campos de escrita para facilitacao do usuario.

--- Secao07/httpreact/src/App.jsx
+++ Secao07/httpreact/src/App.jsx
@@ -9,6 +9,9 @@ function App() {
 
   const [products, setProducts] = useState([])
 
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+
   // 1 - Resgatando dados
 
   useEffect(() => {
@@ -23,10 +26,38 @@ function App() {
     }
 
     fetchData()
-
   }, [])
 
-  console.log(products)
+  // 2 - Adicionando produtos
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    }
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product),
+    })
+
+    // 3 - carregamento dinamico
+
+    const addedProduct = await res.json()
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct])
+
+    setName("")
+    setPrice("")
+
+  }
+
+
 
   return (
     <>
@@ -37,6 +68,27 @@ function App() {
           <li key={product.id}>{product.name} - R$: {product.price} </li>
         ))}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name='name'
+              onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            Preco:
+            <input
+              type="number"
+              value={price}
+              name='price'
+              onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
     </>
   )
 }
