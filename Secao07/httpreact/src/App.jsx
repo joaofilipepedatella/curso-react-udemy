d3c55ccf054f4ca533f31ce9014286c776262f71
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2023-12-22 14:43:29 -0300

    Iniciando aula sobre requisicao HTTP, criacao JSON Server e chamada para o APP.jsx

---
+++ Secao07/httpreact/src/App.jsx
@@ -0,0 +1,44 @@
+
+import './App.css'
+
+import { useState, useEffect } from "react"
+
+const url = "http://localhost:3000/products"
+
+function App() {
+
+  const [products, setProducts] = useState([])
+
+  // 1 - Resgatando dados
+
+  useEffect(() => {
+
+    async function fetchData() {
+      const res = await fetch(url)
+
+      const data = await res.json()
+
+      setProducts(data)
+
+    }
+
+    fetchData()
+
+  }, [])
+
+  console.log(products)
+
+  return (
+    <>
+      <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product) =>
+        (
+          <li key={product.id}>{product.name} - R$: {product.price} </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default App
