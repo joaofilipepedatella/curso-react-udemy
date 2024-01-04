10fa0dbae5194eaf17531c4eaff570319e8837ac
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-01-04 14:14:47 -0300

    Aula sobre config do React Router, Navegacao entre paginas e Carregamento de dados

--- Secao08- React Router/react-router/src/App.jsx
+++ Secao08- React Router/react-router/src/App.jsx
@@ -1,11 +1,31 @@
-
 import './App.css'
 
+//1 - Config react router
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// pages
+
+import Home from './pages/Home'
+import About from './pages/About'
+
+//components
+
+import Navbar from './components/Navbar'
+
+
 function App() {
-  
+
   return (
     <>
       <h1>React Router</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
