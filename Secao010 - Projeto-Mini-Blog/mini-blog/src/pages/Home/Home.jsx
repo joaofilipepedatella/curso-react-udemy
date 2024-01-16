6e97781eed9548b8a1baf9672463fea43e7c44b5
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-01-16 17:41:55 -0300

    criacao do hook para puxar as informacoes do firebase, e postagem na pagina Home

--- Secao010 - Projeto-Mini-Blog/mini-blog/src/pages/Home/Home.jsx
+++ Secao010 - Projeto-Mini-Blog/mini-blog/src/pages/Home/Home.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import { useNavigate, Link } from 'react-router-dom'
-import { useState } from 'react'
-
 //css
 import styles from './Home.module.css'
 
+//hooks
+import { useNavigate, Link } from 'react-router-dom'
+import { useState } from 'react'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+
+//components
+import PostDetail from '../../components/PostDetail'
+
+
+
+
 const Home = () => {
   const [query, setQuery] = useState("")
-  const [posts] = useState([])
+  const { documents: posts, loading } = useFetchDocuments("posts")
 
   const handleSubmit = (e) => {
     e.prevent.default()
@@ -19,12 +26,17 @@ const Home = () => {
     <div className={styles.home}>
       <h1>Veja nossos posts mais recentes</h1>
       <form onSubmit={handleSubmit} className={styles.search_form} >
-        <input type="text" placeholder='Ou busque por tags...' onChange={(e) => setQuery(e.target.value)} />
+        <input
+          type="text"
+          placeholder='Ou busque por tags...'
+          onChange={(e) => setQuery(e.target.value)}
+        />
         <button className='btn btn-dark'>Pesquisar</button>
       </form>
 
       <div>
-        <h1>Posts</h1>
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
         {posts && posts.length === 0 && (
           <div className={styles.noposts}>
             <p>Nao foram encontrados posts</p>
