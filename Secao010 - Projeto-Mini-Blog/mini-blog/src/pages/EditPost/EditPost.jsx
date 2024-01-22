aa3de980b422d4ffab290c6c29cd691b59987ef4
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-01-22 17:29:00 -0300

    Criação do hook de Update de um documento, finalização da página de Edit, finalização do projeto Mini Blog

--- Secao010 - Projeto-Mini-Blog/mini-blog/src/pages/EditPost/EditPost.jsx
+++ Secao010 - Projeto-Mini-Blog/mini-blog/src/pages/EditPost/EditPost.jsx
@@ -3,7 +3,7 @@ import styles from "./EditPost.module.css";
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
-import { useInsertDocument } from "../../hooks/useInsertDocument";
+import { useUpdateDocument } from "../../hooks/useUpdateDocument";
 import { useFetchDocument } from "../../hooks/useFetchDocument";
 
 const EditPost = () => {
@@ -30,7 +30,7 @@ const EditPost = () => {
 
   const { user } = useAuthValue();
 
-  const { insertDocument, response } = useInsertDocument("posts");
+  const { updateDocument, response } = useUpdateDocument("posts");
 
   const navigate = useNavigate();
 
@@ -57,14 +57,16 @@ const EditPost = () => {
 
     if (formError) return;
 
-    insertDocument({
-      title,
-      image,
-      body,
-      tagsArray,
-      uid: user.uid,
-      createdBy: user.displayName,
-    });
+    const data = {
+        title,
+        image,
+        body,
+        tagsArray,
+        uid: user.uid,
+        createdBy: user.displayName,
+    }
+
+    updateDocument(id, data);
 
     //redirect to home page
     navigate("/");
