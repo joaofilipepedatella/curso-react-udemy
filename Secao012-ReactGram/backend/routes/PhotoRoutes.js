c0ae75e8357b45e7bcddf8184547032b12775abd
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-02-05 15:51:20 -0300

    feat: add controller, validation and route to make comments.

--- Secao012-ReactGram/backend/routes/PhotoRoutes.js
+++ Secao012-ReactGram/backend/routes/PhotoRoutes.js
@@ -10,10 +10,16 @@ const {
   getPhotoById,
   updatePhoto,
   likePhoto,
+  commentPhoto,
 } = require("../controllers/PhotoController");
 
 //Middlewares
-const { photoInsertValidation, photoUpdateValidation } = require("../middlewares/photoValidation");
+const {
+  photoInsertValidation,
+  photoUpdateValidation,
+  commentValidation,
+} = require("../middlewares/photoValidation");
+
 const authGuard = require("../middlewares/authGuard");
 const validate = require("../middlewares/handleValidation");
 const { imageUpload } = require("../middlewares/imageUpload");
@@ -33,6 +39,13 @@ router.get("/", authGuard, getAllPhotos);
 router.get("/user/:id", authGuard, getUserPhotos);
 router.get("/:id", authGuard, getPhotoById);
 router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto);
-router.put("/like/:id", authGuard, likePhoto)
+router.put("/like/:id", authGuard, likePhoto);
+router.put(
+  "/comment/:id",
+  authGuard,
+  commentValidation(),
+  validate,
+  commentPhoto
+);
 
 module.exports = router;
