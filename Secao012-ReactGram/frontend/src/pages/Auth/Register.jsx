e0f6d6c10edb43f339477023937dd4c07ceccf59
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-02-08 15:23:05 -0300

    feat: coleta de dados do formulário

--- Secao012-ReactGram/frontend/src/pages/Auth/Register.jsx
+++ Secao012-ReactGram/frontend/src/pages/Auth/Register.jsx
@@ -1,47 +1,66 @@
-import './Auth.css'
+import "./Auth.css";
 
 //Components
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 //Hooks
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 const Register = () => {
-
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [confirmPassword, setConfirmPassword] = useState("")
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const user = {
-      name, 
+      name,
       email,
       password,
       confirmPassword,
-    }
-  }
+    };
 
-
+    console.log(user);
+  };
 
   return (
-    <div id='register'>
+    <div id="register">
       <h2>ReactGram</h2>
-      <p className='subtitle'>Cadastre-se para ver as fotos dos seus amigos!</p>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos!</p>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Nome completo" />
-        <input type="email" placeholder='E-mail' /> 
-        <input type="password" placeholder='Senha' />
-        <input type="password" placeholder='Confirmar senha' />
+        <input
+          type="text"
+          placeholder="Nome completo"
+          onChange={(e) => setName(e.target.value)}
+          value={name || ''}
+        />
+        <input
+          type="email"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || ''}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || ''}
+        />
+        <input
+          type="password"
+          placeholder="Confirmar senha"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          value={confirmPassword || ''}
+        />
         <input type="submit" value="Cadastrar" />
       </form>
       <p>
-        Já possui uma conta? <Link to='/login'>Clique Aqui!</Link>
+        Já possui uma conta? <Link to="/login">Clique Aqui!</Link>
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
