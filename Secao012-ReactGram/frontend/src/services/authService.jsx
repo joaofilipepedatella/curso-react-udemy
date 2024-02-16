f6a7a0c202af45d45b2508a66fdd91ab2ef8cff0
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-02-16 15:24:04 -0300

    feat: add login function

--- Secao012-ReactGram/frontend/src/services/authService.jsx
+++ Secao012-ReactGram/frontend/src/services/authService.jsx
@@ -12,7 +12,7 @@ const register = async (data) => {
         throw err;
       });
 
-    if (res) {
+    if (res._id) {
       localStorage.setItem("user", JSON.stringify(res));
     }
 
@@ -36,7 +36,7 @@ const login = async (data) => {
       .then((res) => res.json())
       .catch((err) => err);
 
-    if (res) {
+    if (res._id) {
       localStorage.setItem("user", JSON.stringify(res));
     }
 
