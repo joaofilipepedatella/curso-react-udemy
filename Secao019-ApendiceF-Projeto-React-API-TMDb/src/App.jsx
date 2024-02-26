ae06209d536fcdaed606040883e0c851edf35515
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-02-26 17:26:08 -0300

    feat: criada home page buscando lista de filmes melhores avaliados da API

--- Secao019-ApendiceF-Projeto-React-API-TMDb/src/App.jsx
+++ Secao019-ApendiceF-Projeto-React-API-TMDb/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Navbar />
+      <Outlet />
     </>
   )
 }
